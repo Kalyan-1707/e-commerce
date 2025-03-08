7452f6eab9245f3aec2d39ac595526518a71abbc
Pavan Kalyan Konudula <konudulapavankalyan@gmail.com>
2025-03-08 17:19:15 -0500

    added create category endpoint

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.project.model.Category;
@@ -17,5 +19,10 @@ public class CategoryController {
         return categories;
     }
 
+    @PostMapping("/api/public/category")
+    public String createCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Category created successfully";
+    }
 
 }

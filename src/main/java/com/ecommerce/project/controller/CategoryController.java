56d57fa9ac8dd0b4f39df4d40976d56680027e30
Pavan Kalyan Konudula <konudulapavankalyan@gmail.com>
2025-03-10 15:58:39 -0400

    added category service

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.ecommerce.project.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +8,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<Category>();
+    public CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/category")
     public String createCategory(@RequestBody Category category){
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category created successfully";
     }
 
---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.service;
+
+import java.util.List;
+
+import com.ecommerce.project.model.Category;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+}
